aeebcf76037655c7704174345622744aad21e117
Lois Truby <loyslane@gmail.com>
2017-12-25 01:29:01 -0500

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Dropdown from 'react-dropdown';
-// import ReactBoard from 'react-board';
 
 import SecondPage from './page-2';
 import './pages.css';
@@ -8,19 +7,6 @@ import './pages.css';
 class IndexPage extends Component {
   constructor() {
     super();
-    // this.board = [
-    //   [a11, a12, a13],
-    //   [b9, b10,b11, b12, b13],
-    //   [c7, c8, c9, c10, c11, c12, c13],
-    //   [d5, d6, d7, d8, d9, d10, d11, d12, d13],
-    //   [e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13],
-    //   [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13],
-    //   [g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13],
-    //   [h5, h6, h7, h8, h9, h10, h11, h12, h13],
-    //   [i7, i8, i9, i10, i11, i12, i13],
-    //   [j9, j10, j11, j12, j13],
-    //   [k11, k12, k13]
-    // ];
     this.state = {
       selectedOption: null,
       numOfPlayers: null,
@@ -32,19 +18,22 @@ class IndexPage extends Component {
       currentPlayer: null,
       rolled: false,
       diceRolls: [],
+      allCombos: [],
+      addedCombos: [],
       chosenComboIndex: null,
-      // num2: {},
-      // num3: {},
-      // num4: {},
-      // num5: {},
-      // num6: {},
-      // num7: {},
-      // num8: {},
-      // num9: {},
-      // num10: {},
-      // num11: {},
-      // num12: {},
-      // white: {}
+      // column array example: 
+      // index 0 = max, index 1 = winner, index 2 = temp, index 3 = blue, index 4 = green, index 5 = orange, index 6 = yellow
+      col2: [3, null, null, null, null, null, null],
+      col3: [5, null, null, null, null, null, null],
+      col4: [7, null, null, null, null, null, null],
+      col5: [9, null, null, null, null, null, null],
+      col6: [11, null, null, null, null, null, null],
+      col7: [13, null, null, null, null, null, null],
+      col8: [11, null, null, null, null, null, null],
+      col9: [9, null, null, null, null, null, null],
+      col10: [7, null, null, null, null, null, null],
+      col11: [5, null, null, null, null, null, null],
+      col12: [3, null, null, null, null, null, null]
     }
     this.handleselectedOption = this.handleselectedOption.bind(this);
     this.onSelect1 = this.onSelect1.bind(this);
@@ -122,7 +111,35 @@ class IndexPage extends Component {
     this.state.diceRolls.push(Math.floor(Math.random() * 6) + 1);
     this.state.diceRolls.push(Math.floor(Math.random() * 6) + 1);
     this.state.diceRolls.push(Math.floor(Math.random() * 6) + 1);
-    this.state.diceRolls.push(Math.floor(Math.random() * 6) + 1); 
+    this.state.diceRolls.push(Math.floor(Math.random() * 6) + 1);
+    const comboPerms = (array) => {
+      const pairPermute = (arr, memo = []) => {
+        let i, current;
+        for (i = 0; i < arr.length; i++) {
+          memo.push(arr[i]);
+        }
+        this.state.allCombos.unshift(memo);
+      }
+      pairPermute(array);
+      let temp = array[1];
+      array[1] = array[2];
+      array[2] = temp;
+      pairPermute(array);
+      temp = array[1];
+      array[1] = array[3];
+      array[3] = temp;
+      pairPermute(array);
+    }
+    comboPerms(this.state.diceRolls);
+    const comboAddition = (arr) => {
+      this.state.addedCombos.push(arr[0][0] + arr[0][1]);
+      this.state.addedCombos.push(arr[0][2] + arr[0][3]);
+      this.state.addedCombos.push(arr[1][0] + arr[1][1]);
+      this.state.addedCombos.push(arr[1][2] + arr[1][3]);
+      this.state.addedCombos.push(arr[2][0] + arr[2][1]);
+      this.state.addedCombos.push(arr[2][2] + arr[2][3]);
+    }
+    comboAddition(this.state.allCombos);
     this.setState({
       rolled: true
     });
@@ -147,38 +164,6 @@ class IndexPage extends Component {
   }
 
   render() {
-    const comboPerms = (array) => {
-      const result = [];
-      const pairPermute = (arr, memo = []) => {
-        let i, current;
-        for (i = 0; i < arr.length; i++) {
-          memo.push(arr[i]);
-        }
-        result.unshift(memo);
-      }
-      pairPermute(array);
-      let temp = array[1];
-      array[1] = array[2];
-      array[2] = temp;
-      pairPermute(array);
-      temp = array[1];
-      array[1] = array[3];
-      array[3] = temp;
-      pairPermute(array);
-      return result;
-    }
-
-    const comboAddition = (arr) => {
-      const result = [];
-      result.push(arr[0][0] + arr[0][1]);
-      result.push(arr[0][2] + arr[0][3]);
-      result.push(arr[1][0] + arr[1][1]);
-      result.push(arr[1][2] + arr[1][3]);
-      result.push(arr[2][0] + arr[2][1]);
-      result.push(arr[2][2] + arr[2][3]);
-      return result;
-    }
-
     let player1Color = <h3 className="colorChoiceText">Please choose player 1 color</h3>;
     let player2Color = <h3 className="colorChoiceText">Please choose player 2 color</h3>;
     let player3Color = <h3 className="colorChoiceText">Please choose player 3 color</h3>;
@@ -276,29 +261,60 @@ class IndexPage extends Component {
     if (this.state.rolled) {
       rollDice = null;
       const rolledNums = this.state.diceRolls;
-      const allCombos = comboPerms(rolledNums);
-      const addedCombos = comboAddition(allCombos);
       diceCombos = 
         <div>
           <div  className="combos">
             <h4>You rolled: {rolledNums[0]}  {rolledNums[1]}  {rolledNums[2]}  {rolledNums[3]}</h4>
             <h3>Please choose a dice combo:</h3>
             <p>Combo 1:</p>
-            <p>{allCombos[0][0]}  {allCombos[0][1]}  /  {allCombos[0][2]}  {allCombos[0][3]}</p>
-            <form className="comboButton" onSubmit={this.handleComboSubmit1}><button type="submit">{addedCombos[0]}   &   {addedCombos[1]}</button></form>
+            <p>{this.state.allCombos[0][0]}  {this.state.allCombos[0][1]}  /  {this.state.allCombos[0][2]}  {this.state.allCombos[0][3]}</p>
+            <form className="comboButton" onSubmit={this.handleComboSubmit1}><button type="submit">{this.state.addedCombos[0]}   &   {this.state.addedCombos[1]}</button></form>
             <p>Combo 2:</p>
-            <p>{allCombos[1][0]}  {allCombos[1][1]}  /  {allCombos[1][2]}  {allCombos[1][3]}</p>
-            <form onSubmit={this.handleComboSubmit2}><button type="submit">{addedCombos[2]}   &   {addedCombos[3]}</button></form>
+            <p>{this.state.allCombos[1][0]}  {this.state.allCombos[1][1]}  /  {this.state.allCombos[1][2]}  {this.state.allCombos[1][3]}</p>
+            <form onSubmit={this.handleComboSubmit2}><button type="submit">{this.state.addedCombos[2]}   &   {this.state.addedCombos[3]}</button></form>
             <p>Combo 3:</p>
-            <p>{allCombos[2][0]}  {allCombos[2][1]}  /  {allCombos[2][2]}  {allCombos[2][3]}</p>
-            <form onSubmit={this.handleComboSubmit3}><button type="submit">{addedCombos[4]}   &   {addedCombos[5]}</button></form>
-{/* 
-            <form onSubmit={this.handleComboSubmit2}><button type="submit">{allCombos[1]}</button></form>
-            <form onSubmit={this.handleComboSubmit3}><button type="submit">{allCombos[2]}</button></form> */}
+            <p>{this.state.allCombos[2][0]}  {this.state.allCombos[2][1]}  /  {this.state.allCombos[2][2]}  {this.state.allCombos[2][3]}</p>
+            <form onSubmit={this.handleComboSubmit3}><button type="submit">{this.state.addedCombos[4]}   &   {this.state.addedCombos[5]}</button></form>
           </div>
         </div>
+      let column2 = <p>column 2: {this.state['col2'][2]}{this.state['col2'][3]}{this.state['col2'][4]}{this.state['col2'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col2'][1]) column2 = <h4>The winner of column 2 is: {this.state[1]}</h4>;
+      let column3 = <p>column 3: {this.state['col3'][2]}{this.state['col3'][3]}{this.state['col3'][4]}{this.state['col3'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col3'][1]) column3 = <h4>The winner of column 3 is: {this.state[1]}</h4>;
+      let column4 = <p>column 4:{this.state['col4'][2]}{this.state['col4'][3]}{this.state['col4'][4]}{this.state['col4'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col4'][1]) column4 = <h4>The winner of column 4 is: {this.state[1]}</h4>;
+      let column5 = <p>column 5:{this.state['col5'][2]}{this.state['col5'][3]}{this.state['col5'][4]}{this.state['col5'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col5'][1]) column5 = <h4>The winner of column 5 is: {this.state[1]}</h4>;
+      let column6 = <p>column 6:{this.state['col6'][2]}{this.state['col6'][3]}{this.state['col6'][4]}{this.state['col6'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col6'][1]) column6 = <h4>The winner of column 6 is: {this.state[1]}</h4>;
+      let column7 = <p>column 7:{this.state['col7'][2]}{this.state['col7'][3]}{this.state['col7'][4]}{this.state['col7'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col7'][1]) column7 = <h4>The winner of column 7 is: {this.state[1]}</h4>;
+      let column8 = <p>column 8:{this.state['col8'][2]}{this.state['col8'][3]}{this.state['col8'][4]}{this.state['col8'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col8'][1]) column8 = <h4>The winner of column 8 is: {this.state[1]}</h4>;
+      let column9 = <p>column 9:{this.state['col9'][2]}{this.state['col9'][3]}{this.state['col9'][4]}{this.state['col9'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col9'][1]) column9 = <h4>The winner of column 9 is: {this.state[1]}</h4>;
+      let column10 = <p>column 10:{this.state['col10'][2]}{this.state['col10'][3]}{this.state['col10'][4]}{this.state['col10'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col10'][1]) column10 = <h4>The winner of column 10 is: {this.state[1]}</h4>;
+      let column11 = <p>column 11:{this.state['col11'][2]}{this.state['col11'][3]}{this.state['col11'][4]}{this.state['col11'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col11'][1]) column11 = <h4>The winner of column 11 is: {this.state[1]}</h4>;
+      let column12 = <p>column 12:{this.state['col12'][2]}{this.state['col12'][3]}{this.state['col12'][4]}{this.state['col12'][5]}{this.state['col2'][6]}</p>;
+      if (this.state['col12'][1]) column12 = <h4>The winner of column 12 is: {this.state[1]}</h4>;
+
       gameBoard = 
-        <div>This is the where game board will go</div>
+        <div>
+          <p>This is a test</p>
+          {column2}
+          {column3}
+          {column4}
+          {column5}
+          {column6}
+          {column7}
+          {column8}
+          {column9}
+          {column10}
+          {column11}
+          {column12}
+        </div>
     }
     
     if (this.state.chosenComboIndex) {
